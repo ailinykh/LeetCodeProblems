e7c4686fadac3b111ee336104aa53838a8725adc
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-02-16 16:22:04 +0400

    Default test case for `main` func

--- Tests/LeetCodeProblemsTests/LeetCodeProblemsTests.swift
+++ Tests/LeetCodeProblemsTests/LeetCodeProblemsTests.swift
@@ -7,5 +7,24 @@ final class LeetCodeProblemsTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(LeetCodeProblems().text, "Let's solve them all!")
+
+        // var expected = "nil"
+        // let pipe = Pipe()
+        // pipe.fileHandleForReading.readabilityHandler = { fileHandle in
+        //     expected = String(data: fileHandle.availableData, encoding: .utf8) ?? "nil"
+        // }
+        // dup2(FileHandle.standardOutput.fileDescriptor, pipe.fileHandleForWriting.fileDescriptor)
+        
+        // LeetCodeProblems.main()
+        // // respore stdout
+        // freopen("/dev/stdout", "a", stdout)
+        // pipe.fileHandleForReading.closeFile()
+
+        // XCTAssertEqual(expected, "Let's solve them all!")
+    }
+
+    @MainActor
+    func testMain() {
+       XCTAssertNoThrow(LeetCodeProblems.main())
     }
 }
