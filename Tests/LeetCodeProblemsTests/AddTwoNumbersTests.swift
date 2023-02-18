e41b3030969f5e6ca125fe5bafb8b025326b0d28
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-02-18 20:03:30 +0300

    2. Add Two Numbers. Variant 2 (#3)

--- Tests/LeetCodeProblemsTests/AddTwoNumbersTests.swift
+++ Tests/LeetCodeProblemsTests/AddTwoNumbersTests.swift
@@ -10,3 +10,32 @@ final class AddTwoNumbersTest: XCTestCase {
         XCTAssertEqual(s.addTwoNumbers([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1].list, [5,6,4].list)?.array, [6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1])
     }
 }
+
+extension AddTwoNumbers.ListNode {
+    public var array: [Int] {
+        var arr = [Int]()
+        var node = AddTwoNumbers.ListNode(0, self)
+        while let next = node.next {
+            arr.append(next.val)
+            node = next
+        }
+        return arr
+    }
+}
+
+extension Array where Element == Int {
+    public var list: AddTwoNumbers.ListNode? {
+        var first, last: AddTwoNumbers.ListNode?
+        for n in self {
+            if last != nil {
+                let node = AddTwoNumbers.ListNode(n)
+                last?.next = node
+                last = node
+            } else {
+                first = AddTwoNumbers.ListNode(n)
+                last = first
+            }
+        }
+        return first
+    }
+}
