779107bfe08b49779495209348b4279ac7e1e629
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-02-18 20:55:34 +0400

    2. Add Two Numbers. Variant 2

--- Sources/LeetCodeProblems/AddTwoNumbers.swift
+++ Sources/LeetCodeProblems/AddTwoNumbers.swift
@@ -3,73 +3,46 @@
 
 import Foundation
 
-
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-}
-
 public class AddTwoNumbers {
+    public class ListNode {
+        public var val: Int
+        public var next: ListNode?
+        public init() { self.val = 0; self.next = nil; }
+        public init(_ val: Int) { self.val = val; self.next = nil; }
+        public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    }
+    
     public init() {}
 
     public func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        let list1 = l1?.array ?? []
-        let list2 = l2?.array ?? []
-        let (min, max) = list1.count > list2.count ? (list2, list1) : (list1, list2)
-        var result = [Int]()
-        
+        let result = ListNode()
+        var node = result
+        var firstNode: ListNode? = ListNode(0, l1)
+        var secondNode: ListNode? = ListNode(0, l2)
         var hasPeriod = false
-        for (i, r) in max.enumerated() {
-            var l = 0
-            if (i < min.count) {
-                l = min[i]
-            }
-            var sum = r + l
+        while firstNode?.next != nil || secondNode?.next != nil {
+            firstNode = firstNode?.next
+            secondNode = secondNode?.next
+            
+            var sum = (firstNode?.val ?? 0) + (secondNode?.val ?? 0)
             if hasPeriod {
                 hasPeriod = false
                 sum = sum + 1
             }
+            
             if sum > 9 {
                 hasPeriod = true
                 sum = sum % 10
             }
-            result.append(sum)
-        }
-        if hasPeriod {
-            result.append(1)
+            
+            node.next = ListNode(sum)
+            node = node.next!
         }
         
-        return result.list
-    }
-}
-
-extension ListNode {
-    public var array: [Int] {
-        var arr = [Int]()
-        var node = ListNode(0, self)
-        while let next = node.next {
-            arr.append(next.val)
-            node = next
+        if hasPeriod {
+            node.next = ListNode(1)
         }
-        return arr
-    }
-}
-
-extension Array where Element == Int {
-    public var list: ListNode? {
-        var first, last: ListNode?
-        for n in self {
-            if last != nil {
-                let node = ListNode(n)
-                last?.next = node
-                last = node
-            } else {
-                first = ListNode(n)
-                last = first
-            }
-        }
-        return first
+        
+        return result.next
     }
 }
