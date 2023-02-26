c8d0d4cca760bf5fd38e218a4e7a3dd380eb045b
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-02-26 10:14:11 +0400

    Add one more test case

--- Tests/LeetCodeProblemsTests/CheckIfArrayIsSortedAndRotatedTests.swift
+++ Tests/LeetCodeProblemsTests/CheckIfArrayIsSortedAndRotatedTests.swift
@@ -9,6 +9,7 @@ final class CheckIfArrayIsSortedAndRotatedTest: XCTestCase {
         XCTAssertEqual(s.check([1,2,3]), true)
         XCTAssertEqual(s.check([1]), true)
         XCTAssertEqual(s.check([7,9,1,1,1,3]), true)
+        XCTAssertEqual(s.check([7,9,1,1,1,3,1]), false)
         XCTAssertEqual(s.check([6,10,6]), true)
     }
 }
