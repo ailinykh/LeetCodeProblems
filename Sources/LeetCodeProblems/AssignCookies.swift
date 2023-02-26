5b876668819d00c4b4bde1515c0c545c9389be67
Anthony Ilinykh <anthonyilinykh@gmail.com>
2023-02-26 11:16:04 +0400

    code refactoring

--- Sources/LeetCodeProblems/AssignCookies.swift
+++ Sources/LeetCodeProblems/AssignCookies.swift
@@ -10,10 +10,7 @@ public final class AssignCookies {
     public func findContentChildren(_ g: [Int], _ s: [Int]) -> Int {
         let g = g.sorted(), s = s.sorted()
         var i = 0, j = 0, count = 0
-        while true {
-            guard i < g.count, j < s.count else  {
-                return count
-            }
+        while i < g.count && j < s.count {
             if g[i] <= s[j] {
                 i += 1
                 count += 1
